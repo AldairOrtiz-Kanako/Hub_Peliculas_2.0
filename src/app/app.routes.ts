e79ea8001708a2b31f3ad73d539624e9a6419c84
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-08-16 11:20:28 -0600

    Refactoring code
    Co-authored-by: Julieta171 <Julieta171@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,23 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Login/login/login.component';
 import { RegistroComponent } from './Registro/registro/registro.component';
 import { InicioComponent } from './Inicio/inicio/inicio.component';
-import { SeriesComponent } from './Inicio/Series/series/series.component';
-import { PeliculasComponent } from './Inicio/Peliculas/peliculas/peliculas.component';
-import { FavoritosComponent } from './Inicio/Favoritos/favoritos/favoritos.component';
 import { PerfilComponent } from './Perfil/perfil/perfil.component';
+import { FavoritosComponent } from './Favoritos/favoritos/favoritos.component';
+import { PeliculasComponent } from './Peliculas/peliculas/peliculas.component';
+import { SeriesComponent } from './Series/series/series.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
-  {
-    path: 'inicio',
-    component: InicioComponent,
-    children: [
-      { path: 'series', component: SeriesComponent },
-      { path: 'peliculas', component: PeliculasComponent },
-      { path: 'favoritos', component: FavoritosComponent },
-    ],
-  },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'series', component: SeriesComponent },
+  { path: 'peliculas', component: PeliculasComponent },
+  { path: 'favoritos', component: FavoritosComponent },
   { path: 'perfil', component: PerfilComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
