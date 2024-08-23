3d7cf6990d0e176fdc1c47f4bf0d79451e9be6c5
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-08-22 18:34:45 -0600

    Adding components header and sidebar on inicio page

--- src/app/Inicio/inicio/inicio.component.ts
+++ src/app/Inicio/inicio/inicio.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [NgIf, RouterLink],
+  imports: [NgIf, RouterLink, SidebarComponent, HeaderComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
 })
