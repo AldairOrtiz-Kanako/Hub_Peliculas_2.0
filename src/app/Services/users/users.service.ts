8c51b421ca70cc2f8d9f6dbc1fa22b3336c0f4b5
AldairOrtiz9850 <60710532+AldairOrtiz9850@users.noreply.github.com>
2024-08-23 21:20:16 -0600

    Cambios al servicio para la obtención de series y peliculas

--- src/app/Services/users/users.service.ts
+++ src/app/Services/users/users.service.ts
@@ -2,16 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
+export interface CatalogoItem {
+  tipo: string;
+  titulo: string;
+  temporadas?: number;
+  episodios?: number;
+  fechaEstreno: Date;
+  sinopsis: string;
+  genero: string;
+  director: string;
+  poster: string;
+  trailer: string;
+  duracion?: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  private apiUrl = 'http://localhost:5025/api/User'; // URL del backend
+  private apiUrl = 'http://localhost:5025/api';
   private http = inject(HttpClient);
 
+  constructor() {}
+
   registerUser(userData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}`, userData);
+    return this.http.post(`${this.apiUrl}/User`, userData);
   }
 
-  constructor() {}
+  getCatalogoCompleto(): Observable<CatalogoItem[]> {
+    return this.http.get<CatalogoItem[]>(`${this.apiUrl}/PeliculasySeries`);
+  }
 }
